974fa4f6f68d71cac29fcb24768dea3a31d07b7b
javierborjas <cristman11@gmail.com>
2023-11-09 00:16:36 -0300

    Creando el objeto principal para validar y sincronizar datos

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+    const email = {
+        email: '',
+        asunto: '',
+        mensaje: ''
+    }
+
+    
+
     // Seleccionar los elementos de la interfaz
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
@@ -23,6 +31,12 @@ document.addEventListener('DOMContentLoaded', function() {
         };
 
         limpiarAlerta(e.target.parentElement);
+
+        // Asignar los valores
+        email[e.target.name] = e.target.value.trim().toLowerCase();
+
+        // Comprobar el objeto de email
+        comprobarEmail();
 
     }
 
@@ -50,7 +64,10 @@ document.addEventListener('DOMContentLoaded', function() {
         const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ 
         const resultado = regex.test(email);
         return resultado;
+    }
 
+    function comprobarEmail() {
+        console.log(Object.values(email).includes(''));
     }
 
 });
