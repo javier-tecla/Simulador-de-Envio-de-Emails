96fd5240db6cb19c8132092e8f41f7e2916094c2
javierborjas <cristman11@gmail.com>
2023-11-10 00:13:22 -0300

    Mostrando el Spinner cuando se envia el email y reiniciar el formulario y Mostrando una alerta de éxito

--- js/app.js
+++ js/app.js
@@ -13,23 +13,47 @@ document.addEventListener('DOMContentLoaded', function() {
     const formulario = document.querySelector('#formulario');
     const btnSubmit = document.querySelector('#formulario button[type="submit"]');
     const btnReset = document.querySelector('#formulario button[type="reset"]');
+    const spinner = document.querySelector('#spinner');
 
     // Asignar eventos
     inputEmail.addEventListener('input', validar);
     inputAsunto.addEventListener('input', validar);
     inputMensaje.addEventListener('input', validar);
 
+    formulario.addEventListener('submit', enviarEmail);
+
     btnReset.addEventListener('click', function(e) {
         e.preventDefault();
 
-        // reiniciar el objeto
-        email.email = '';
-        email.asunto = '';
-        email.mensaje = '';
-
-        formulario.reset();
-        comprobarEmail();
+        resetFormulario();
+       
     });
+
+    function enviarEmail(e) {
+        e.preventDefault();
+
+        spinner.classList.add('flex');
+        spinner.classList.remove('hidden');
+
+        setTimeout(() => {
+            spinner.classList.remove('flex');
+            spinner.classList.add('hidden');
+
+            resetFormulario();
+
+            // Crear una alerta
+            const alertaExito = document.createElement('P');
+            alertaExito.classList.add('bg-green-500', 'text-white', 'p-2', 'text-center', 'rounded-lg', 'mt-10', 'font-bold', 'text-sm', 'uppercase');
+            alertaExito.textContent = 'Mensaje enviado correctamente';
+
+            formulario.appendChild(alertaExito);
+
+            setTimeout(() => {
+                alertaExito.remove();
+            }, 3000);
+
+        }, 3000);
+    }
     
     function validar(e) {
         if(e.target.value.trim() === '') {
@@ -91,6 +115,16 @@ document.addEventListener('DOMContentLoaded', function() {
             btnSubmit.classList.remove('opacity-50');
             btnSubmit.disabled = false;   
         
+    }
+
+    function resetFormulario() {
+        // reiniciar el objeto
+            email.email = '';
+            email.asunto = '';
+            email.mensaje = '';
+ 
+         formulario.reset();
+         comprobarEmail();
     }
 
 });
