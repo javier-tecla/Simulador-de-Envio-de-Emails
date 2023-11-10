b2f77bb785ce44e4eabaaa4fa0a82598016c5691
javierborjas <cristman11@gmail.com>
2023-11-09 21:27:29 -0300

    Habilitar o deshabilitar el botón de enviar y Resetear el Formulario

--- js/app.js
+++ js/app.js
@@ -5,28 +5,44 @@ document.addEventListener('DOMContentLoaded', function() {
         asunto: '',
         mensaje: ''
     }
-
     
-
     // Seleccionar los elementos de la interfaz
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
     const formulario = document.querySelector('#formulario');
+    const btnSubmit = document.querySelector('#formulario button[type="submit"]');
+    const btnReset = document.querySelector('#formulario button[type="reset"]');
 
     // Asignar eventos
-    inputEmail.addEventListener('blur', validar);
-    inputAsunto.addEventListener('blur', validar);
-    inputMensaje.addEventListener('blur', validar);
+    inputEmail.addEventListener('input', validar);
+    inputAsunto.addEventListener('input', validar);
+    inputMensaje.addEventListener('input', validar);
+
+    btnReset.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        // reiniciar el objeto
+        email.email = '';
+        email.asunto = '';
+        email.mensaje = '';
+
+        formulario.reset();
+        comprobarEmail();
+    });
     
     function validar(e) {
         if(e.target.value.trim() === '') {
             mostrarAlerta(`El Campo ${e.target.id} es obligatorio`, e.target.parentElement);
+            email[e.target.name] = '';
+            comprobarEmail();
             return;
         } 
 
         if(e.target.id === 'email' && !validarEmail(e.target.value)) {
             mostrarAlerta('El email no es válido', e.target.parentElement);
+            email[e.target.name] = '';
+            comprobarEmail();
             return;
         };
 
@@ -67,7 +83,14 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function comprobarEmail() {
-        console.log(Object.values(email).includes(''));
+        if(Object.values(email).includes('')) {
+            btnSubmit.classList.add('opacity-50');
+            btnSubmit.disabled = true;
+            return;
+        } 
+            btnSubmit.classList.remove('opacity-50');
+            btnSubmit.disabled = false;   
+        
     }
 
 });
